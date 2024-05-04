a91aef7f1f65fb2eee069b1d7d0ff8b934ad0fb7
Joel-Liju <joelliju10@gmail.com>
2024-05-04 18:16:27 -0400

    cleaned up and now it is a package

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import json
 
 path = "./recipes/"
 
-url = "https://www.indianhealthyrecipes.com/chicken-biryani-recipe/"
+#url = "https://www.indianhealthyrecipes.com/chicken-biryani-recipe/"
+url = "https://www.myhouseofpizza.com/poolish-pizza-dough-recipe/"
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
+items = soup.find("script",type = "application/ld+json")# this is what it looks for
+jsonObject = json.loads(items.get_text())
 
-ingredients = soup.find_all("li",class_="")
-
-for ingredient in ingredients:
-	print(ingredient)
+for item in jsonObject["@graph"]:
+	if "Recipe" in item["@type"]:
+		print(item["recipeInstructions"])
